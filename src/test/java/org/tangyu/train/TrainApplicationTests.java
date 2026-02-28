09ba281a3eec9232ebeec469192f5154bfac7516
dddxmx <2069133272@qq.com>
2026-02-28 15:57:22 +0800

    初始化

---
+++ src/test/java/org/tangyu/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tangyu.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
